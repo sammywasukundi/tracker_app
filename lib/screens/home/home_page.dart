f06466f32e95acb86c6ab1de54852ec2896a4733
sammywasukundi <brosammy1et2@gmail.com>
2024-09-20 02:21:45 +0200

    I'm starting design of home_page

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,5 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:tracker_app/read%20datas/get_user_name.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,29 +16,319 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String? imageUrl;
+
   @override
   Widget build(BuildContext context) {
     // Récupération de l'utilisateur actuel
     User? user = FirebaseAuth.instance.currentUser;
 
-    return Scaffold(
-      body: Center(
+    //doc IDs
+    List<String> docIDs = [];
+
+    //get docs
+    Future getDocId() async {
+      await FirebaseFirestore.instance
+          .collection('users')
+          //.orderBy('first name', descending: true)
+          .get()
+          .then((snapshot) => snapshot.docs.forEach(
+                (document) {
+                  print(document.reference);
+                  docIDs.add(document.reference.id);
+                },
+              ));
+    }
+
+    // @override
+    // void initState() {
+    //   getDocId();
+    //   super.initState();
+    // }
+
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 10,
+          vertical: 10,
+        ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Affichage de l'email ou d'un texte par défaut si l'utilisateur est null
-            Text('Connected as ${user?.email ?? 'Null'}'),
-            const SizedBox(height: 20),
-            MaterialButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              color: Colors.blueAccent[400],
-              child: const Text('Sign out'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.yellow[700],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 60,
+                          child: ClipOval(
+                            child: imageUrl != null
+                                ? Image.network(
+                                    imageUrl!, // Afficher l'image si imageUrl n'est pas nul
+                                    width: double.infinity,
+                                    fit: BoxFit.cover,
+                                  )
+                                : Icon(Icons.person,
+                                    size:
+                                        40,), // Afficher une icône par défaut si imageUrl est nul
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Bienvenue sur Tracker_app !",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Theme.of(context).colorScheme.outline,
+                            ),
+                          ),
+                          Text(
+                            user!.email!,
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                IconButton(
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut();
+                  },
+                  icon: Icon(
+                    Icons.logout,
+                    size: 35,
+                  ),
+                )
+              ],
             ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width / 2,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: const [
+                      Color.fromARGB(255, 192, 221, 215),
+                      Color.fromARGB(255, 89, 156, 211),
+                      Color.fromARGB(255, 160, 172, 177),
+                    ],
+                    transform: const GradientRotation(pi / 4),
+                  ),
+                  borderRadius: BorderRadius.circular(25),
+                  boxShadow: const [
+                    BoxShadow(
+                        blurRadius: 4,
+                        color: const Color.fromARGB(255, 243, 242, 242),
+                        offset: const Offset(5, 5))
+                  ]),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Total Balance',
+                    style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  const Text(
+                    '\$ 2000.00',
+                    style: TextStyle(
+                        fontSize: 40.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 12, horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                                width: 25,
+                                height: 25,
+                                decoration: const BoxDecoration(
+                                    color: Colors.white30,
+                                    shape: BoxShape.circle),
+                                child: const Center(
+                                  child: Icon(
+                                    Icons.arrow_downward,
+                                    size: 12,
+                                    color: Colors.greenAccent,
+                                  ),
+                                )),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            const Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Income',
+                                  style: TextStyle(
+                                      fontSize: 14.0,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                Text(
+                                  '\$ 200.00',
+                                  style: TextStyle(
+                                      fontSize: 14.0,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                                width: 25,
+                                height: 25,
+                                decoration: const BoxDecoration(
+                                    color: Colors.white30,
+                                    shape: BoxShape.circle),
+                                child: const Center(
+                                  child: Icon(
+                                    Icons.arrow_upward,
+                                    size: 12,
+                                    color: Colors.redAccent,
+                                  ),
+                                )),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            const Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Expenses',
+                                  style: TextStyle(
+                                      fontSize: 14.0,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                Text(
+                                  '\$ 400.00',
+                                  style: TextStyle(
+                                      fontSize: 14.0,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Transactions',
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      color: Theme.of(context).colorScheme.onBackground,
+                      fontWeight: FontWeight.bold),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    'View all',
+                    style: TextStyle(
+                        fontSize: 14.0,
+                        color: Theme.of(context).colorScheme.outline,
+                        fontWeight: FontWeight.w400),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+                child: FutureBuilder(
+                    future: getDocId(),
+                    builder: (context, snapshot) {
+                      return ListView.builder(
+                        itemCount: docIDs.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.all(6.0),
+                            child: ListTile(
+                              title: GetUserName(documentId: docIDs[index]),
+                              tileColor: Colors.grey[300],
+                            ),
+                          );
+                        },
+                      );
+                    }))
           ],
         ),
       ),
+      //   body: Center(
+      //     child: Column(
+      //       mainAxisAlignment: MainAxisAlignment.center,
+      //       children: [
+      //         // Affichage de l'email ou d'un texte par défaut si l'utilisateur est null
+      //         //Text('Connected as ${user.email ?? 'Null'}'),
+      //         const SizedBox(height: 20),
+      //         Expanded(
+      //           child: FutureBuilder(
+      //             future: getDocId(),
+      //             builder: (context, snapshot) {
+      //               return ListView.builder(
+      //                 itemCount: docIDs.length,
+      //                 itemBuilder: (context, index) {
+      //                   return Padding(
+      //                     padding: const EdgeInsets.all(12.0),
+      //                     child: ListTile(
+      //                       title: GetUserName(documentId: docIDs[index]),
+      //                       tileColor: Colors.grey[300],
+      //                     ),
+      //                   );
+      //                 },
+      //               );
+      //             }
+
+      //           )
+      //         )
+      //       ],
+      //     ),
+      //   ),
     );
   }
 }
